6cf686933a5cdcb4f2bc0cf0498fbbd66a39b63a
Jeremy Jácome <jeremyja2006@gmail.com>
2025-07-17 18:28:28 -0500

    Delete proyecto_final_sql_flask_v3 directory

--- proyecto_final_sql_flask_v3/models/database.py
+++
@@ -1,9 +0,0 @@
-"""
-Base de datos y configuración central
-"""
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-
-# Instancias globales
-db = SQLAlchemy()
-bcrypt = Bcrypt()
